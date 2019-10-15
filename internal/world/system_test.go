ec886080951183f4ce1a604a1b9ab7bbc2171f5f
Horst Gutmann <zerok@zerokspot.com>
2019-10-15 16:55:34 +0200

    Test the System.ShellOutput helper

---
+++ internal/world/system_test.go
@@ -0,0 +1,48 @@
+package world_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/zerok/tpl/internal/world"
+)
+
+// TestSystemShellOutput checks that ShellOutput moves the rendering into an
+// error state if the executed command fails.
+func TestSystemShellOutput(t *testing.T) {
+	w := world.New(&world.Options{
+		Insecure: true,
+	})
+	tests := []struct {
+		input   string
+		output  string
+		errored bool
+	}{
+		{
+			input:   "{{ .System.ShellOutput \"i-dont-exist\" }}",
+			output:  "",
+			errored: true,
+		},
+		{
+			input:   "{{ .System.ShellOutput \"echo hello\" }}",
+			output:  "hello\n",
+			errored: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			var out bytes.Buffer
+			in := bytes.NewBufferString(test.input)
+			err := w.Render(&out, in)
+			if test.errored {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.output, out.String())
+			}
+		})
+
+	}
+}
