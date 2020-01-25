3e43f03c9989d442af7411eacc09a6ea0fa58d89
Horst Gutmann <zerok@zerokspot.com>
2020-01-25 20:56:59 +0100

    Refactor the fs testcases to be more concise

--- internal/world/fs_test.go
+++ internal/world/fs_test.go
@@ -3,17 +3,28 @@ package world
 import (
 	"bytes"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFSExists(t *testing.T) {
 	w := New(&Options{})
+
+	t.Run("existing-file", func(t *testing.T) {
+		out := requireRender(t, w, `{{ if .FS.Exists "world.go" }}exists{{ else }}not found{{ end }}`)
+		require.Equal(t, "exists", out)
+	})
+
+	t.Run("not-existing-file", func(t *testing.T) {
+		out := requireRender(t, w, `{{ if .FS.Exists "missing-file.go" }}exists{{ else }}not found{{ end }}`)
+		require.Equal(t, "not found", out)
+	})
+}
+
+func requireRender(t *testing.T, w *World, tpl string) string {
 	var out bytes.Buffer
-	in := bytes.NewBufferString(`{{ if .FS.Exists "world.go" }}exists{{ else }}not found{{ end }}`)
+	in := bytes.NewBufferString(tpl)
 	err := w.Render(&out, in)
-	if err != nil {
-		t.Fatalf("rendering shouldn't have resulted in an error. Got %s instead", err.Error())
-	}
-	if out.String() != "exists" {
-		t.Fatalf("Unexpected output: %v", out.String())
-	}
+	require.NoError(t, err)
+	return out.String()
 }
